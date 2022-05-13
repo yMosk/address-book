59d072fe5c7b0f6dd8369e810e47404781675a1d
Yana Moskalova <88861368+yMosk@users.noreply.github.com>
2022-05-13 14:30:55 -0700

    feat: eslint fix (#4)

--- src/components/AddressesList.jsx
+++ src/components/AddressesList.jsx
@@ -15,6 +15,7 @@ export default function AddressesList() {
       .catch((error) => {
         console.log(error);
       });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const onGetAllSuccess = (response) => {
